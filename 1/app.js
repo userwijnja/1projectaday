2aae919610919fa7a6d8be9366393396a2bc5582
Myrthe Wijnja <mwijnja@gmail.com>
2022-02-10 10:59:25 +0100

    initial commit

---
+++ 1/app.js
@@ -0,0 +1,10 @@
+//dag 1
+//https://www.youtube.com/watch?v=qy9catMEUL4
+function beSad(){
+    document.getElementById('myImage').src='sad.png'
+}
+
+function beHappy(){
+    document.getElementById('myImage').src='happy.jpg';
+
+}
---
+++ 3/app.js
@@ -0,0 +1,6 @@
+function generateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById( 'flex-cat-gen');
+image.src = "Catgenerator.jpg"
+div.appendChild(image);
+}
